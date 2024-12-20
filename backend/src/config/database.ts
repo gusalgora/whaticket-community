21a3bbc0be2a537c4f43e58195f1e24d0b66b42f
dockerdavid <jdgomezdelgado@uts.edu.co>
2024-12-20 17:27:01 -0500

    nuevas correcciones de whaticket

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,13 +3,14 @@ require("../bootstrap");
 module.exports = {
   define: {
     charset: "utf8mb4",
-    collate: "utf8mb4_bin"
+    collate: "utf8mb4_general_ci",
   },
   dialect: process.env.DB_DIALECT || "mysql",
-  timezone: "-03:00",
+  timezone: "-05:00",
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  logging: false
+  logging: false,
+  port: process.env.DB_PORT,
 };
